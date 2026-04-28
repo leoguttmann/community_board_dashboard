5384b8a02e75bfde02de43f229aa22aa1875d3f4
Tyler Green <tyler@fromscratchcode.com>
2026-04-28 13:21:22 -0400

    Update deploy_lambda.sh

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+# Usage: ./deploy_lambda.sh [--new]
+#   Default: updates the existing Lambda function code.
+#   --new: creates the Lambda function for the first time.
 
-# Replace with your AWS Lambda function name and file paths
+set -e
+
 FUNCTION_NAME="CBFunction"
 ZIP_FILE_PATH="cbpackage.zip"
 HANDLER="lambda_app.lambda_handler"
+NEW=false
 
-# Package the Lambda function code
+for arg in "$@"; do
+  case $arg in
+    --new) NEW=true ;;
+  esac
+done
+
 (cd ./app && zip -r $ZIP_FILE_PATH ./*)
 
+if [ "$NEW" = true ]; then
+  echo "Creating new Lambda function..."
+  aws lambda create-function \
+    --profile mcb7 \
+    --region us-east-1 \
+    --function-name $FUNCTION_NAME \
+    --runtime python3.8 \
+    --role arn:aws:iam::239460480281:role/cb-dashboard-data-store-lambda-role \
+    --handler $HANDLER \
+    --zip-file fileb://app/$ZIP_FILE_PATH
+else
+  echo "Updating Lambda function code..."
+  aws lambda update-function-code \
+    --profile mcb7 \
+    --region us-east-1 \
+    --function-name $FUNCTION_NAME \
+    --zip-file fileb://app/$ZIP_FILE_PATH
+fi
 
-# Deploy or update the Lambda function
-# aws lambda create-function \
-#     --function-name $FUNCTION_NAME \
-#     --runtime python3.8 \
-#     --role arn:aws:iam::212905216651:role/cb-dashboard-data-store-lambda-role \
-#     --handler $HANDLER \
-#     --zip-file fileb://$ZIP_FILE_PATH
-
-# Optionally, update the Lambda function code (if you have already created it)
-aws lambda update-function-code \
-   --function-name $FUNCTION_NAME \
-   --zip-file fileb://app/$ZIP_FILE_PATH
-
-# Clean up the temporary package
 rm ./app/$ZIP_FILE_PATH
